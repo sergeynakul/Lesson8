38ade87e2ed45ff6851a82b5a4b6a5db58f28607
Sergio <nakul.sv@gmail.com>
2019-04-18 06:46:52 +0300

    refactor speed

--- passenger_train.rb
+++ passenger_train.rb
@@ -5,9 +5,9 @@ class PassengerTrain < Train
   end
 
   def attach_carriage(carriage)
-    @carriages << carriage if carriage.type == :passenger && !@carriages.include?(carriage) && @speed == 0
+    @carriages << carriage if carriage.type == :passenger && !@carriages.include?(carriage) && @speed.zero?
   end
   def detach_carriage(carriage)
-    @carriages.delete(carriage) if carriage.type == :passenger && @carriages.include?(carriage) && @speed == 0
+    @carriages.delete(carriage) if carriage.type == :passenger && @carriages.include?(carriage) && @speed.zero?
   end
 end
